421bfdfa71f6e9c7540c96c792867b3864074440
anhduc123 <anhducn1297@gmail.com>
2019-04-24 22:28:09 +0700

    menu Scene

---
+++ Assets/Scripts/Menu/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+    private const string coin = "coin";
+    private const string sound = "sound";
+    private const string music = "music";
+
+    private void Awake()
+    {
+        MakeInstance();
+        FirstTime();
+
+    }
+
+    private void Start()
+    {
+    }
+
+    void MakeInstance()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void FirstTime()
+    {
+        if (!PlayerPrefs.HasKey("_FirstTime"))
+        {
+            SetCoin(0);
+            SetMusic("on");
+            SetSound("on");
+            PlayerPrefs.SetInt("_FirstTime", 0);
+        }
+    }
+
+    public void SetCoin(int number)
+    {
+        PlayerPrefs.SetInt(coin, number);
+    }
+
+    public int GetCoin()
+    {
+        return PlayerPrefs.GetInt(coin);
+    }
+
+    public void SetSound(string state)
+    {
+        PlayerPrefs.SetString(sound, state);
+    }
+
+    public string GetSound()
+    {
+        return PlayerPrefs.GetString(sound);
+    }
+
+
+    public void SetMusic(string state)
+    {
+        PlayerPrefs.SetString(music, state);
+    }
+
+    public string GetMusic()
+    {
+        return PlayerPrefs.GetString(music);
+    }
+
+
+}
---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    public static MenuController instance;
+    [Header("UI")]
+    public Button btnAddCoin;
+    public Button btnPlay, btnShop, btnSetting, btnRate, 
+        btnRank, btnShare, btnExitPopup, btnSound, btnMusic, btnNoAD;
+    public GameObject popUpSetting;
+    public Text textCoin;
+
+    [Header("Sprite")]
+    public Sprite music;
+    public Sprite musicP, noMusic, noMusicP, sound, soundP, noSound, noSoundP;
+
+
+    private void Start()
+    {
+        Init();
+        InitStateButton();
+    }
+
+    public void Init()
+    {
+        //Init button listener
+        btnAddCoin.onClick.AddListener(OnClickAddCoin);
+        btnPlay.onClick.AddListener(OnClickPlay);
+        btnShop.onClick.AddListener(OnClickShop);
+        btnRate.onClick.AddListener(OnClickRate);
+        btnRank.onClick.AddListener(OnClickRank);
+        btnShare.onClick.AddListener(OnClickShare);
+        btnExitPopup.onClick.AddListener(OnClickExitPopupSetting);
+        btnSound.onClick.AddListener(OnClickSound);
+        btnMusic.onClick.AddListener(OnClickMusic);
+        btnNoAD.onClick.AddListener(OnClickNoAD);
+        btnSetting.onClick.AddListener(OnClickSetting);
+
+    }
+
+    #region OnClickButton
+
+    void OnClickAddCoin()
+    {
+
+    }
+
+    void OnClickPlay()
+    {
+
+    }
+
+    void OnClickShop()
+    {
+
+    }
+
+    void OnClickSetting()
+    {
+        popUpSetting.SetActive(true);
+    }
+
+    void OnClickRate()
+    {
+
+    }
+
+    void OnClickRank()
+    {
+
+    }
+
+    void OnClickShare()
+    {
+
+    }
+
+    void OnClickExitPopupSetting()
+    {
+        popUpSetting.SetActive(false);
+    }
+
+    void OnClickSound()
+    {
+        if (GameManager.instance.GetSound() == "on")
+        {
+            ChangeSpriteBtn(btnSound, noSound, noSoundP);
+            GameManager.instance.SetSound("off");
+        }
+        else
+        {
+            ChangeSpriteBtn(btnSound, sound, soundP);
+            GameManager.instance.SetSound("on");
+        }
+    }
+
+    void OnClickMusic()
+    {
+        if(GameManager.instance.GetMusic() == "on")
+        {
+            ChangeSpriteBtn(btnMusic, noMusic, noMusicP);
+            GameManager.instance.SetMusic("off");
+        }
+        else
+        {
+            ChangeSpriteBtn(btnMusic, music, musicP);
+            GameManager.instance.SetMusic("on");
+        }
+    }
+
+    void OnClickNoAD()
+    {
+
+    }
+
+    #endregion
+
+    #region Help
+
+    void InitStateButton()
+    {
+        if (GameManager.instance.GetSound() == "on") ChangeSpriteBtn(btnSound, sound, soundP);
+        else ChangeSpriteBtn(btnSound, noSound, noSoundP);
+
+        if (GameManager.instance.GetMusic() == "on") ChangeSpriteBtn(btnMusic, music, musicP);
+        else ChangeSpriteBtn(btnMusic, noMusic, noMusicP);
+
+    }
+
+    void ChangeSpriteBtn(Button btn, Sprite img, Sprite imgP)
+    {
+        btn.GetComponent<Image>().sprite = img;
+
+        SpriteState state = btn.spriteState;
+        state.pressedSprite = imgP;
+
+        btn.spriteState = state;
+
+    }
+
+    #endregion
+
+
+}
